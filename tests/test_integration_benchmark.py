7f13efe478cc402b61f07b8f526cf456bcbdde28
刘永锋 <272462809@qq.com>
2025-08-11 07:14:05 +0800

    [Enhancement] Adding Notification Capabilities to Integration Testing (#85)
    * [Enhancement] Adding Notification Capabilities to Integration Testing
    
    * Fix the number of integration test tasks

--- tests/test_integration_benchmark.py
+++ tests/test_integration_benchmark.py
@@ -6,10 +6,8 @@ import pytest
 
 from datus.agent.agent import Agent
 from datus.configuration.agent_config import AgentConfig
-from datus.schemas.node_models import SqlTask
 from datus.tools.db_tools.db_manager import DBManager, db_manager_instance
 from datus.utils.benchmark_utils import load_bird_dev_tasks
-from datus.utils.constants import DBType
 from tests.conftest import load_acceptance_config
 
 
@@ -24,26 +22,30 @@ def db_manager(agent_config: AgentConfig) -> DBManager:
 
 
 def test_benchmark_bird(agent_config: AgentConfig, db_manager: DBManager):
-    args = argparse.Namespace(
-        **{"components": ["metrics", "metadata", "table_lineage", "document"], "load_cp": None, "max_steps": 10}
-    )
-    agent = Agent(args=args, agent_config=agent_config, db_manager=db_manager)
     # random some task_ids
     tasks = load_bird_dev_tasks(agent_config.benchmark_path("bird_dev"))
-    task_ids = set(generate_unique_random_numbers(10, len(tasks)))
-    for task in tasks:
-        task_id = task["question_id"]
-        if task_id not in task_ids:
-            continue
-        sql_task = SqlTask(
-            id=str(task_id),
-            database_type=DBType.SQLITE,
-            task=task["question"],
-            database_name=task["db_id"],
-            external_knowledge=task["evidence"],
-            output_dir="tests/output/bird_dev",
-        )
-        agent.run(sql_task=sql_task, check_storage=True)
+    task_ids = set(generate_unique_random_numbers(30, len(tasks)))
+    args = argparse.Namespace(
+        **{
+            "components": ["metrics", "metadata", "table_lineage", "document"],
+            "load_cp": None,
+            "max_steps": 10,
+            "benchmark": "bird_dev",
+            "namespace": "bird_sqlite",
+            "benchmark_task_ids": [str(item) for item in task_ids],
+            "benchmark_path": None,
+            "task_db_name": "",
+            "task_schema": "",
+            "metric_meta": "",
+            "domain": "",
+            "layer1": "",
+            "layer2": "",
+            "task_ext_knowledge": "",
+        }
+    )
+    agent = Agent(args=args, agent_config=agent_config, db_manager=db_manager)
+
+    agent.benchmark()
 
 
 def generate_unique_random_numbers(count, max_val) -> List[int]:
